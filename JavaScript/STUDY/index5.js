fe4fe2ccec14d987a4753c006c045289cb62c742
sice11 <tensyoku.yonezawa@gmail.com>
2020-05-20 21:52:28 +0900

    continue文を使う　から

--- JavaScript/STUDY/index5.js
+++ JavaScript/STUDY/index5.js
@@ -1,6 +1,20 @@
+//let state = true;
+//while(state){
+    //let random = Math.floor(Math.random()*10);
+    //if(random === 3) state = false;
+    //console.log(random);
+//}
+
+//
+
+//for ( let i = 0; i < 5; i++ ){
+    //console.log(i);
+    //if(i === 3 ) break;
+//}
+
 let state = true;
 while(state){
     let random = Math.floor(Math.random()*10);
-    if(random === 3) state = false;
+    if(random === 3)break;
     console.log(random);
 }
