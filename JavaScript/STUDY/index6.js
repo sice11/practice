03304fe557e2a60068a8a389efbb40258d4ce16f
sice11 <tensyoku.yonezawa@gmail.com>
2020-05-21 22:07:52 +0900

    6章途中（関数宣言と関数式の違い）から

---
+++ JavaScript/STUDY/index6.js
@@ -0,0 +1,13 @@
+//関数宣言
+//function myFunc(){
+    //処理内容
+//}
+
+function myFunc(){
+    console.log("これは一番目の関数です。");
+}
+function myFunc(){
+    console.log("これは二番目の関数です。");
+}
+
+console.log(myFunc);
