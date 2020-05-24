a8522e6e26daeddf8a0123f1e7af98b77295f990
sice11 <tensyoku.yonezawa@gmail.com>
2020-05-24 17:19:44 +0900

    7章 課題の回答

--- JavaScript/TASK/kadai7.js
+++ JavaScript/TASK/kadai7.js
@@ -26,6 +26,50 @@ let atPoint = Math.floor(Math.random()*10);
 //     alert("無事に逃げました。");
 // }
 
-for (let enemyHP = 10; enemyHP<0; enemyHP-atPoint){
-    console.log(enemyHP);
-}
+// for (let enemyHP = 10; enemyHP<0; enemyHP-atPoint){
+//     console.log(enemyHP);
+// }
+
+
+//回答
+// 自キャラのオブジェクト
+const hero = {
+    name: '太郎',
+    hp: 30,
+    attack: function() {
+      return Math.floor(Math.random() * 10)
+    }
+  }
+  
+  // 敵キャラのオブジェクト
+  const enemy = {
+    name: 'スライム',
+    hp: 15
+  };
+  
+  
+  while(true) {
+    const command = prompt(enemy.name + 'が現れた！（【1】攻撃　【2】逃げる）');
+    
+    // [攻撃]を選択した場合
+    if(command === '1') {
+      const attack = hero.attack();
+      
+      enemy.hp = enemy.hp - attack;
+      console.log(hero.name + 'は敵に' + attack + 'のダメージを与えた！');
+      if(enemy.hp <=0) {
+        console.log('敵を倒した！');
+        break;
+      }
+      console.log('敵の体力は残り' + enemy.hp + 'です');
+  
+    // [逃げる]を選択した場合
+    } else if(command === '2') {
+      
+      console.log('無事に逃げました');
+      break;
+      
+    }
+  }
+  
+  console.log('ゲーム終了');
