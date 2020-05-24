9648809e583488a18778bfaf7ba6e18af4a191d9
sice11 <tensyoku.yonezawa@gmail.com>
2020-05-24 19:11:43 +0900

    7章 復習の途中

---
+++ JavaScript/TASK/review/kadai7.js
@@ -0,0 +1,22 @@
+//7章の課題の復習
+
+let hero = {
+    name:"Tarou",
+    HP:30,
+    attack:function atPoint(){
+        return Math.floor(Math.random()*10);
+    }
+}
+
+let enemy = {
+    name:"スライム",
+    HP:15
+}
+
+
+let command = prompt(enemy.name + "が現れた！　【1】攻撃　【2】逃げる")
+if (command == String(1)||"1"){
+    console.log(hero.name + "が" + enemy.name + "に" + hero.attack() +　"の攻撃");
+}else if(command == String(2)||"2"){
+    console.log(hero.name + "は逃げた。");
+}
