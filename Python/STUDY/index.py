d767dba6dc50faaa0f329d39b77d5d4f50ca0bc3
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-06 23:42:51 +0900

    #3完了

--- Python/STUDY/index.py
+++ Python/STUDY/index.py
@@ -2,12 +2,25 @@
 # print("Hellow World!")
 
 #2
-name = "名前 と　苗字"
+# name = "名前 と　苗字"
 # print(name)
 
 # num = 1
 # print(num + num)
 
-def func():
-    print(name)
-func()
+# def func():
+#     print(name)
+# func()
+
+#3
+# def func(str):
+#     print(str)
+# func("Hello World!")
+
+# def func(a,b):
+#     print(a * b)
+# func(12,16)
+
+def calc(a,b):
+    print(a * b)
+calc(12,16)
