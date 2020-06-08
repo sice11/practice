69685e7a03851d095a09648da05d3f0e45d067e4
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-08 13:08:25 +0900

    #7for課題完了

---
+++ Python/TASK/for.py
@@ -0,0 +1,14 @@
+# sum = 0
+# for i in range(1,21):
+#     sum += i
+# print(sum)
+
+# 課題
+# for num in range(1,31):
+#     if num % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(num)
+
+for num2 in range(1,31,2):
+    print(num2)
