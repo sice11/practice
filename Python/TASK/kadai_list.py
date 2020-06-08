0d4d5a7fb5060623ece3fa295bdabb40821e5abb
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-08 10:49:13 +0900

    #6課題完了

---
+++ Python/TASK/kadai_list.py
@@ -0,0 +1,26 @@
+# listの作成、追加、削除、値の取得を行う
+
+list = ["one","two","three","four"]
+
+# 値の取得
+# print(list[1])
+# print(list[1:3])
+
+# 値の追加
+# list.append("five")
+# print(list)
+
+# 値の削除
+# すべての値を削除
+# list.clear()
+# print(list)
+
+# 指定した位置の要素を削除
+# list.pop(1)
+# print(list)
+
+# 指定した要素を削除。ただし、複数同じ値があった場合は、
+# 最初の値だけが削除される
+# list2 = ["a","b","c","a","d"]
+# list2.remove("a")
+# print(list2)
