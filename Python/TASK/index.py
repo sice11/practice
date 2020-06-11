51a60b47b0b16fcf3fd9bddd802a6828bd3a66a4
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-11 16:29:22 +0900

    paizaの項目 index.pyに実行（未完成）

---
+++ Python/TASK/index.py
@@ -0,0 +1,20 @@
+#ある制の整数nが入力される
+#正の整数1から9に整数nをそれぞれを掛けた数を半角スペース区切りで出力して下さい。
+#条件 1 ≦ n ≦ 100
+
+# import random
+
+# num = 1
+
+# while num <= 9:
+#     n = random.randint(1,100)
+#     print(n * num,end=" ")
+#     num += 1
+
+n = int()
+
+num = 1
+
+while num <= 9:
+    print(n * num,end=" ")
+    num += 1
