de56e371e8f51e9cd65c215a2fa0c2f5027206ce
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-12 09:31:40 +0900

    RPG3.pyにてclass実装かつ完成形

--- Python/TASK/free/RPG3.py
+++ Python/TASK/free/RPG3.py
@@ -32,27 +32,50 @@ while (hiro_hp > 0 and enemy_hp > 0):
 
     # 属性値による優劣を決める
     # 属性値反映後の攻撃値を以下にする
-    hiroAT = hiro.attack
-    enemyAT = enemy.attack
-
-    # hiroの攻撃値が0.5倍になるケースをまとめる
-    if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
-        (hiroAT == hiroAT * 0.5) and (enemyAT == enemyAT * 1.5)
-    #hiroの攻撃値が1.5倍になるケースをまとめる
-    elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
-        (hiroAT == hiroAT * 1.5) and (enemyAT == enemyAT * 0.5)
-    # 等倍分
-    else:
-        (hiroAT == hiroAT * 1.0) and (enemyAT == enemyAT * 1.0)
+    class attackPT:
+        # 不利属性の場合の倍率
+        Hpoint_05 = hiro.attack * 0.5
+        Epoint_05 = enemy.attack * 0.5
+        # 有利属性の場合の倍率
+        Hpoint_15 = hiro.attack * 1.5
+        Epoint_15 = enemy.attack * 1.5
+        # 同属性の場合の倍率
+        Hpoint_1 = hiro.attack * 1.0
+        Epoint_1 = enemy.attack * 1.0
     
+    # 主人公の属性値反映後の実際の攻撃値
+    def hiroAT():
+        # hiroの攻撃値が0.5倍になるケースをまとめる
+        if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
+            return attackPT.Hpoint_05
+        #hiroの攻撃値が1.5倍になるケースをまとめる
+        elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
+            return attackPT.Hpoint_15
+        # 等倍分
+        else:
+            return attackPT.Hpoint_1
+    
+    # 敵の属性値反映後の実際の攻撃値
+    def enemyAT():
+        # hiroの攻撃値が0.5倍になるケースをまとめる
+        if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
+            return attackPT.Epoint_15
+        #hiroの攻撃値が1.5倍になるケースをまとめる
+        elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
+            return attackPT.Epoint_05
+        # 等倍分
+        else:
+            return attackPT.Epoint_1
 
+    # 戦闘の計算を以下で行う。
+    # 出力される攻撃値は、属性倍率を反映しない値を表記
     # 主人公を先行
-    enemy_hp -= hiroAT
-    print("hiroが",hiro.attribute,"属性で",hiroAT,"の攻撃")
+    enemy_hp -= hiroAT()
+    print("hiroが",hiro.attribute,"属性で",hiro.attack,"の攻撃")
     print("enemyの残り体力は",enemy_hp)
     # 敵の後攻
-    hiro_hp -= enemyAT
-    print("enemyが",enemy.attribute,"属性で",enemyAT,"の攻撃")
+    hiro_hp -= enemyAT()
+    print("enemyが",enemy.attribute,"属性で",enemy.attack,"の攻撃")
     print("hiroの残り体力は",hiro_hp)
 
 if hiro_hp > 0:
