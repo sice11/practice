1c91eb373a00f2c4bd96253e94b1e61b8320a164
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-15 09:34:10 +0900

    classに属性と攻撃値、属性反映後の攻撃値をまとめる

--- Python/TASK/free/RPG2.py
+++ Python/TASK/free/RPG2.py
@@ -1,55 +1,82 @@
 import random
 
-# 属性の設定
-attribute = ["火","水","雷"]
-i = random.randint(0,2)
+# 主人公と敵の体力を入力
+hiro_hp = 120
+enemy_hp = 200
 
-# 各ステータスの元を作成
-class Status:
-    def __init__(self,name,hp,attack,attribute):
-        self.name = name
-        self.hp = hp
-        self.attack = attack
-        self.attribute = attribute[i]
-    def __call__(self):
-        print("{}が{}属性で{}の攻撃".format(self.name,self.attribute,self.attack))
-
-#主人公と敵のステータスを入力
-hiro = Status("hiro",120,random.randint(20,40),attribute)
-enemy = Status("enemy",50,random.randint(10,25),attribute)
+print("戦闘開始")
 
 # 先行を主人公とする
 # 以下から、戦闘を開始。どちらかのhpの値が0になるまで実行
-while(hiro.hp > 0) or (enemy.hp > 0):
+while (hiro_hp > 0 and enemy_hp > 0):
 
-    # 属性値による優劣を決める
-    # 属性値反映後の攻撃値を以下にする
-    hiroAT = hiro.attack
-    enemyAT = enemy.attack
+    #主人公の攻撃力と属性を入力
+    class hiro:
+        # 属性値
+        attributeH = ["火","水","雷"]
+        h = random.randint(0,2)
+        attribute = attributeH[h]
+        # 攻撃値
+        attack = random.randint(20,40)
+        # 属性倍率反映後の攻撃値
+        # 不利属性の場合
+        Hpoint_05 = attack * 0.5
+        # 有利属性の場合
+        Hpoint_15 = attack * 1.5
+        # 同属性の場合
+        Hpoint_1 = attack * 1.0
+        # 主人公の属性値反映後の実際の攻撃値
+        def hiroAT():
+            # hiroの攻撃値が0.5倍になるケースをまとめる
+            if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
+                return hiro.Hpoint_05
+            #hiroの攻撃値が1.5倍になるケースをまとめる
+            elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
+                return hiro.Hpoint_15
+            # 等倍分
+            else:
+                return hiro.Hpoint_1
 
-    # hiroの攻撃値が0.5倍になるケースをまとめる
-    if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
-        (hiroAT * 0.5) and (enemyAT * 1.5)
-    #hiroの攻撃値が1.5倍になるケースをまとめる
-    elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
-        (hiroAT * 1.5) and (enemyAT * 0.5)
-    # 等倍分
-    else:
-        (hiroAT * 1.0) and (enemyAT * 1.0)
+    # 敵の攻撃力と属性を入力
+    class enemy:
+        attributeE = ["火","水","雷"]
+        e = random.randint(0,2)
+        # 攻撃値
+        attack = random.randint(10,15)
+        attribute = attributeE[e]
+        # 属性倍率反映後の攻撃値
+        # 不利属性の場合
+        Epoint_05 = attack * 0.5
+        # 有利属性の場合
+        Epoint_15 = attack * 1.5
+        # 同属性の場合
+        Epoint_1 = attack * 1.0
+        def enemyAT():
+            # hiroの攻撃値が0.5倍になるケースをまとめる
+            if (hiro.attribute == "火" and enemy.attribute == "水") or (hiro.attribute == "水" and enemy.attribute == "雷") or (hiro.attribute == "雷" and enemy.attribute == "火"):
+                return enemy.Epoint_15
+            #hiroの攻撃値が1.5倍になるケースをまとめる
+            elif (hiro.attribute == "水" and enemy.attribute == "火") or (hiro.attribute == "雷" and enemy.attribute == "水") or (hiro.attribute == "火" and enemy.attribute == "雷"):
+                return enemy.Epoint_05
+            # 等倍分
+            else:
+                return enemy.Epoint_1
     
-    print("戦闘開始")
+    
+    # 敵の属性値反映後の実際の攻撃値
 
+    # 戦闘の計算を以下で行う。
+    # 出力される攻撃値は、属性倍率を反映しない値を表記
     # 主人公を先行
-    # callメソッドの呼び出し
-    hiro()
-    enemy.hp -= hiroAT
-
+    enemy_hp -= hiro.hiroAT()
+    print("hiroが",hiro.attribute,"属性で",hiro.attack,"の攻撃")
+    print("enemyの残り体力は",enemy_hp)
     # 敵の後攻
-    # callメソッドの呼び出し
-    enemy()
-    hiro.hp -= enemyAT
+    hiro_hp -= enemy.enemyAT()
+    print("enemyが",enemy.attribute,"属性で",enemy.attack,"の攻撃")
+    print("hiroの残り体力は",hiro_hp)
 
-if hiro.hp > 0:
-    print(hiro.name + "の勝ち")
+if hiro_hp > 0:
+    print("hiroの勝ち")
 else:
-    print(enemy.name + "の勝ち")
+    print("enemyの勝ち")
