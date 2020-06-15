78b379a0eb6ce3d46dd03074252dd470ab584d60
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-15 09:01:34 +0900

    for文への書き直し　失敗

--- Python/TASK/review/mynavi11.py
+++ Python/TASK/review/mynavi11.py
@@ -3,13 +3,29 @@
 # print(set(list))
 
 # 演習2
+# d = {}
+# input_word = input()
+# word_list = input_word.split(" ")
+# if word_list[0] == "save":
+#     d[word_list[1]] = word_list[2]
+#     print(d)
+# elif word_list[0] == "get":
+#     print(d[word_list[1]])
+# else:
+#     print("Error")
 d = {}
-input_word = input()
-word_list = input_word.split(" ")
-if word_list[0] == "save":
-    d[word_list[1]] = word_list[2]
-    print(d)
-elif word_list[0] == "get":
-    print(d[word_list[1]])
-else:
-    print("Error")
+for k,v in d.items():
+    input_word = input()
+    word_list = input_word.split(" ")
+    if (word_list[0] == "save" and len(word_list) == 3):
+        k = word_list[1]
+        v = word_list[2]
+        d[k] = v
+    elif (word_list[0] == "get" and len(word_list) == 2):
+        k = word_list[1]
+        if k in d:
+            print(d[k])
+        else:
+            print("Error")
+    else:
+        print("Input Error")
