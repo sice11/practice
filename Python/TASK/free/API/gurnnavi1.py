65279934fc98244b1467ec8b75310d2f30b7ed4a
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-20 01:11:05 +0900

    APIテスト

--- Python/TASK/free/API/gurnnavi1.py
+++ Python/TASK/free/API/gurnnavi1.py
@@ -5,19 +5,23 @@ import pprint
 
 # レストラン検索APIのURL
 URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-
-keyid_input = input()
+KEYID_input = input()
 
 # リクエストパラメータの設定
 params = {}
 # keyidは後に外部入力にする
-params["keyid"] = keyid_input
+params["keyid"] = KEYID_input
 params["hit_per_page"] = 20
 # 以下のフリーワードも外部入力
 params["freeword"] = "大阪駅,魚"
 
 # URLにリクエストする
 json_response = requests.get(URL,params).text
+# 基本jsonで考えて、csv出力したとき(printで出力するなど)して、
+# 文字化け・表示形式がおかしい場合はjsonではない可能性があるので、
+# そこで調べる。
+# →APIの扱っているサイトを見て調べる
+# APIのリファレンスを見る
 # json形式のレスポンスをパース（変換）して、辞書型にする。
 res = json.loads(json_response)
 
