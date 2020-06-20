62c9187ad0adc5650ae12eed5fca606000ccae82
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-21 01:32:42 +0900

    2章完了

--- Ruby/STUDY/hello.rb
+++ Ruby/STUDY/hello.rb
@@ -1,3 +1,19 @@
 # puts "Hello World!"
 
-puts "Hello Ruby!"
+# puts "Hello Ruby!"
+
+# 第二章
+# str = "Hello World!"
+# puts str
+
+# name = "Sice"
+# puts name
+
+class Hoge
+    def num
+        @@num_num = 4 * 5
+        puts @@num_num
+    end
+end
+hoge = Hoge.new
+puts hoge.num
