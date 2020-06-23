a108141913b3137585eef7837c57146ed450e6b3
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-23 10:29:10 +0900

    issues #27

--- Ruby/TASK/shopping.rb
+++ Ruby/TASK/shopping.rb
@@ -3,27 +3,16 @@
 # 今月がボーナス時期かどうかを取得する
 # 決められた値以外の場合は繰り返し入力をする
 def check
-    input = gets.chomp.to_i
-    case input
-        when 1
-            puts "ボーナス月ですね。"
-        when 2
-            puts "通常給与月ですね。"
+    puts "数字の1か2を入れてください。"
+    inputs_flag = true
+    while(inputs_flag)
+        input = gets.chomp.to_i
+        if input == 1 || input == 2
+            puts input == 1 ? "ボーナス月ですね。" : "通常給与月ですね。"
+            inputs_flag = false
         else
-            input_flag = true
-            while (input_flag)
-                case input
-                    when 1
-                        puts "ボーナス月ですね。"
-                        input_flag = false
-                    when 2
-                        puts "通常給与月ですね。"
-                        input_flag = false
-                    else
-                        puts "数字の1または2を入力してください"
-                        input = gets.chomp.to_i
-                end
-            end
+            puts "数字の1か2を入れてください。"
+        end
     end
 end
 check
