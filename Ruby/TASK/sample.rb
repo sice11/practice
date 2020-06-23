f02354c3f1f77a4be6afb454edb30826736ce000
sice11 <tensyoku.yonezawa@gmail.com>
2020-06-23 16:22:06 +0900

    値段のメッセージ追加

--- Ruby/TASK/sample.rb
+++ Ruby/TASK/sample.rb
@@ -4,13 +4,14 @@ def list
     while(inputs_flag2)
         puts "買いたいものの名前を入力。終わる場合はendを入力。"
         input_name = gets
-        if input_name = "end"
-            puts "入力を終了します。"
-            inputs_flag2 = false
-        else
+        if input_name != "end"
+            puts "その値段を入力してください。"
             input_value = gets.chomp.to_i
             want_list[:name] = input_name
             want_list[:value] = input_value
+        else
+            puts "入力を終了します。"
+            inputs_flag2 = false
         end
     end
     puts want_list
